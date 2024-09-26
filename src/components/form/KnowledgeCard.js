656b0773ea749bf62e919e34e62e5420fd2373f3
Rafael Meira <rafaelmeiranascimento@hotmail.com>
2024-09-26 17:18:49 -0300

    Vegeta

---
+++ src/components/form/KnowledgeCard.js
@@ -0,0 +1,17 @@
+import styles from "./KnowledgeCard.module.css"
+import { color, motion } from "framer-motion";
+
+function KnowledgeCard({ img, titulo }) {
+    return (
+        <motion.div className={styles.content} initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x: 0}} whileHover={{scale: 1.2}} transition={{duration: 0.2, delay: 0.2}} viewport={{once:true}}>
+            <div className={styles.image} >
+                <img src={img} alt={titulo} className={styles.img}/>
+            </div>
+            <div className={styles.texto}>
+                <h2>{titulo}</h2>
+            </div>
+        </motion.div>
+    )
+}
+
+export default KnowledgeCard
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,11 +8,13 @@ import ProjectCard from "../form/ProjectCard";
 import { useEffect, useState } from "react";
 
 import { color, motion } from "framer-motion";
+import KnowledgeCard from "../form/KnowledgeCard";
 
 function Home() {
 
   const [loading, setLoading] = useState(true)
   const [projeto, setProjeto] = useState([])
+  const [knowledge, setKnowledge] = useState([])
   const [aboutMe, setAboutMe] = useState(false)
 
   useEffect(() => {
@@ -24,66 +26,73 @@ function Home() {
     })
     .then((resp) => resp.json())
     .then((data) => {
-      setLoading(false)
       setProjeto(data.sort((a, b) => a.id - b.id).slice(0, 6));
+    })
+    .catch((err) => console.log(err))
+
+    fetch("http://localhost:5000/Knowledge", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setLoading(false)
+      setKnowledge(data)
     })
     .catch((err) => console.log(err))
   },[])
 
   return (
     <div className={styles.home_container}>
-      <section
-        id="apresentacao"
-        className={styles.content}
-        style={{
-          backgroundImage: `url(${background})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
+      <section id="apresentacao" className={styles.content}>
         <div className={styles.section}>
-          {aboutMe
-          ?
-          <h1>teste</h1>
-          :
           <motion.div className={styles.text} initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, delay: 0.5}} viewport={{once: true}}>
-            <h1>
-              <span>R</span>AFAE
-              <span>L</span>
-            </h1>
-            <h1>
-              <span>M</span>EIR
-              <span>A</span>
-            </h1>
+            <h1>Olá, eu me chamo <span>Rafael Meira!</span></h1>
+            <p>Sou desenvolvedor Web, com foco em Back e Front end.</p>
+            <h2>Para transitar entre informações pessoais e profissionais, <span onClick={() => setAboutMe(!aboutMe)} style={{ cursor: "pointer" }}>clique aqui.</span></h2>
           </motion.div>
-          }
-          
-          <motion.div initial={{opacity: 0, scale: 0.5}} animate={{opacity: 1, scale: 1}} whileHover={{scale: 1.1}} transition={{duration: 0.5, delay: 0.3, ease: [0, 0.71, 0.2, 1.01]}}>
-            <img src={me} alt="Rafael" className={styles.image} onClick={() => setAboutMe(!aboutMe)}/>
+        <div className={styles.imageContainer}>
+        {!aboutMe && 
+          <>
+          <motion.div className={styles.aboutMe} initial={{opacity: 0, x: -100, scale: 1}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.5, delay: 0.4}} viewport={{once:true}}>
+            <h1>Sobre mim</h1>
+            <p>
+              Trabalhando na área de suporte, tive inúmeras experiências onde tive que resolver problema sobre pressão. Junto a isso, eu tive inúmeras experiências com desenvolvimento de projetos nos lugares onde trabalhei, onde trabalhei programando para Mobile me utilizando de tecnologias como React Native (programando em Typescript) ou projetos em Excel me utilizando do VBA.
+            </p>
           </motion.div>
-        </div>
-        <motion.div className={styles.especializacao} initial={{opacity: 0, y:-50}} animate={{opacity: 1, y: 0}} transition={{duration: 0.8, delay: 0.8}}>
-          <h1>PROGRAMADOR WEB</h1>
-        </motion.div>
-      </section>
-      <section id="aboutMe" className={styles.content_color2}>
-        <motion.div className={styles.title} initial={{opacity: 0, scale: 0.5}} whileInView={{opacity: 1, scale: 1}} transition={{duration: 1, delay: 0.1}} viewport={{once:true}}>
-          <h1>SOBRE MIM</h1>
-        </motion.div>
-        <div className={styles.section_2}>
-          <motion.div className={styles.aboutMe} initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.5, delay: 0.4}} viewport={{once:true}}>
+          </>}
+          <motion.img initial={{opacity: 0, scale: 0.5}} animate={aboutMe ? {x: [-100, -10], opacity: 1, scale: 1} : {x: [100, 10], opacity: 1, scale: 1}} whileHover={{scale: 1.1}} transition={{duration: 1, ease: "easeInOut"}} src={me} alt="Rafael" className={styles.image} onClick={() => setAboutMe(!aboutMe)}/>
+          {aboutMe && 
+          <>
+          <motion.div className={styles.aboutMe} initial={{opacity: 0, x: -100, scale: 1}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.5, delay: 0.4}} viewport={{once:true}}>
             <h1>RESUMO PROFISSIONAL</h1>
             <p>
               Trabalhando na área de suporte, tive inúmeras experiências onde tive que resolver problema sobre pressão. Junto a isso, eu tive inúmeras experiências com desenvolvimento de projetos nos lugares onde trabalhei, onde trabalhei programando para Mobile me utilizando de tecnologias como React Native (programando em Typescript) ou projetos em Excel me utilizando do VBA.
             </p>
-            <h1>FOCO PROFISSIONAL</h1>
-            <p>
-              Procuro me expecializar em linguagem WEB, onde no momento procuro aperfeiçoar minhas habilidades com Javascript, HTML e CSS. Junto a isso, estou desenvolvendo projetos para aperfeiçoar ainda mais meus conhecimentos sobre API's, Banco de Dados relacionais e não relacionais, além de estar focado em melhorar minhas habilidades com testes individuais. Em paralelo, estou também estudando inglês e buscando a fluência na língua.
-            </p>
           </motion.div>
-          <motion.div initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.5, delay: 0.4}} viewport={{once:true}}>
-            <img src={icon} alt="Icon" className={styles.icon} />
-          </motion.div>
+          </>}
+        </div>
+        </div>
+      </section>
+      <section id="aboutMe" className={styles.content}>
+        <motion.div className={styles.title} initial={{opacity: 0, scale: 0.5}} whileInView={{opacity: 1, scale: 1}} transition={{duration: 1, delay: 0.1}} viewport={{once:true}}>
+          <h1>Conhecimentos</h1>
+          <p>Estabeleci um conhecimento concreto com varias tecnologias, onde pude aplica-las em projetos, tais como <span>React Native, React, Typescript, Javascript, Firebase, SQL, VBA, </span>entre outras.</p>
+        </motion.div>
+        <div className={styles.conhecimentos_Container}>
+          {!loading ?
+          knowledge.map((knowledge) => (
+            <KnowledgeCard 
+            img={knowledge.img}
+            titulo={knowledge.titulo}
+            />
+          ))
+          :
+          <h1>Loading</h1>
+          }
+          <h1>hi</h1>
         </div>
       </section>
       <section id="Project" className={styles.content}>
