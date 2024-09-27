41488070499c9d8a1990c21ed02106128eb1ab6c
Rafael Meira <rafaelmeiranascimento@hotmail.com>
2024-09-27 15:10:08 -0300

    Update ProjectCard.js

--- src/components/form/ProjectCard.js
+++ src/components/form/ProjectCard.js
@@ -5,8 +5,8 @@ import { motion } from "framer-motion"
 function ProjectCard({ img, titulo, desc, codigo, link }) {
     return (
         <motion.div className={styles.projeto} initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x: 0}} whileHover={{scale: 1.1}} transition={{duration: 0.2, delay: 0.2}} viewport={{once:true}}>
-            <div className={styles.projeto_imagem} >
-                <img src={img} alt={titulo} className={styles.img} href={link}/>
+            <div className={styles.projeto_imagem} onClick={() => window.open(link)}>
+                <img src={img} alt={titulo} className={styles.img}/>
             </div>
             <div className={styles.projeto_texto}>
                 <h2>{titulo}</h2>
